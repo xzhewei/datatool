1cae22c2ad0c5326d67a261c94493dba1ca4a2de
xuzhewei <xzhewei@gmail.com>
2019-06-02 17:08:18 +0800

    gpc struct update

--- script/eval_roadline_script.m
+++ script/eval_roadline_script.m
@@ -1,13 +1,17 @@
 function eval_roadline_script
-caltech_gpp = load('E:\Work\Work_scut\论文\05.ROI\exp\caltech\S1-G1-7-frcnn_VGG16_C4-roadline\detectron-output\test\caltech_test_1x_roadline\generalized_rcnn\roadline.mat');
+% caltech_gpp = load('E:\Work\Work_scut\论文\05.ROI\exp\caltech\S1-G1-7-frcnn_VGG16_C4-roadline\detectron-output\test\caltech_test_1x_roadline\generalized_rcnn\roadline.mat');
+caltech_gpp = load('E:\Code\maskrcnn-benchmark\output\caltech\190411-S1-G1-8-e2e_frcnn_VGG_16_C4_gpn_8_gpu-Area\inference-noslice-prop\caltech_test_1x_roadline\gpc.mat');
 caltech_gt_all = load('E:\Datasets\caltech\mat\test30\gt-All.mat');
-scut_gpp = load('E:\Work\Work_scut\论文\05.ROI\exp\scut\S1-G1-4-frcnn_VGG16_C4-roadline-acr\detectron-output\test\scut_test_1x_roadline_bak\generalized_rcnn\roadline.mat');
+% scut_gpp = load('E:\Work\Work_scut\论文\05.ROI\exp\scut\S1-G1-4-frcnn_VGG16_C4-roadline-acr\detectron-output\test\scut_test_1x_roadline_bak\generalized_rcnn\roadline.mat');
+scut_gpp = load('E:\Code\maskrcnn-benchmark\output\scut\190411-S1-G1-8-e2e_frcnn_VGG_16_C4_gpn_8_gpu-Area\inference-noslice\scut_test_1x_roadline\gpc.mat');
 scut_gt_all = load('E:\Datasets\scut\mat\test25\gt-All.mat');
 
 
-caltech_gpp_center = double(caltech_gpp.roadline');
+% caltech_gpp_center = double(caltech_gpp.roadline');
+caltech_gpp_center = caltech_gpp.all_gpcs(:,1);
 [caltech_gpp_err] = eval_roadline(caltech_gpp_center,caltech_gt_all.gt);
-scut_gpp_center = double(scut_gpp.roadline');
+% scut_gpp_center = double(scut_gpp.roadline');
+scut_gpp_center = scut_gpp.all_gpcs(:,1);
 [scut_gpp_err] = eval_roadline(scut_gpp_center,scut_gt_all.gt);
 
 index = caltech_gpp_err.index;
@@ -51,7 +55,7 @@ hold(axes1,'on');
 % 使用 plot 的矩阵输入创建多行
 plot1 = plot(YMatrix1,'LineWidth',1);
 set(plot1(1),'DisplayName','GT','Color',[0 0.6 0]);
-set(plot1(2),'DisplayName','GPP','Color',[1 0 0]);
+set(plot1(2),'DisplayName','GAP-center','Color',[1 0 0]);
 
 
 % 创建 legend
