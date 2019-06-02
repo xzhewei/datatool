95e74532922ef45354bcc068e655afa29a9ae90c
xuzhewei <xzhewei@gmail.com>
2019-06-02 17:08:44 +0800

    move set to input args

--- util/drawBBox.m
+++ util/drawBBox.m
@@ -1,12 +1,14 @@
-function drawBBox(s,v,f,bbox,col,ls,lw)
+function [fig] = drawBBox(set,s,v,f,bbox,col,ls,lw)
+% drawwBBox('usa',7,10,200,bbox,'r','-',2)
+% 从数据集set中�?择set07_V010_I00199帧按照bbox绘制图片
 s = s+1;
 v = v+1;
-[pthl,setIds,vidIds] = dbInfo('scut');
+[pthl,setIds,vidIds] = dbInfo(set);
 pth = pthl;
 name=sprintf('set%02d/V%03d',setIds(s),vidIds{s}(v));
 sr=seqIo([pth '/videos/' name '.seq'],'reader'); info=sr.getinfo();
 sr.seek(f-1); I=sr.getframe();
-fig = figure(11);imshow(I,'border','tight');
+fig = figure;imshow(I,'border','tight');
 if isempty(lw)
     lw = 2;
 end
