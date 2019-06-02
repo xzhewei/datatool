a59ed104e1d2cadc2fb953647d268cec1f164fa9
xuzhewei <xzhewei@gmail.com>
2019-06-02 17:42:47 +0800

    ignore alglist

--- caltech/alglist.m
+++ caltech/alglist.m
@@ -1,7 +1,34 @@
 function algs = alglist()
 n=1000; clrs=zeros(n,3);
 for i=1:n, clrs(i,:)=max(.3,mod([78 121 42]*(i+1),255)/255); end
+% algs = CSP(clrs);
+algs = FCOS(clrs);
+
+end
+
+function algs = paper_GPCAnet(clrs)
 algs = {  
-  'frcnn_VGG16_C4',       0, clrs(6,:),   '-'
+  'ACF',       0, clrs(6,:),   '-'
+  'AdaptFasterRCNN',       0, clrs(9,:),   '-'
+%   'GPCAnet',       0, clrs(12,:),   '-'
+  'MS-CNN',       0, clrs(10,:),   '--'
+  'RPN+BF',       0, clrs(11,:),   '--'
+  'FasterR-CNN',  0, clrs(7,:),  '--'
+%   'GPCAnet-filter',       0, clrs(12,:),   '-'
+  'FRCNN-ratio2', 0, clrs(12,:), '-'
+};
+end
+
+function algs = CSP(clrs)
+algs = {  
+  'CSP-h-off-079',       0, clrs(6,:),   '-'
+};
+end
+
+function algs = FCOS(clrs)
+algs = {  
+  'FCOS_new',       0, clrs(6,:),   '-'
+  'RPN+BF',     0, clrs(7,:), '_'
+  'RetinaNet',     0, clrs(8,:), '_'
 };
 end
