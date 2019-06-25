e14746be66e1e859ffc82397e94703a3abfa3945
xuzhewei <xzhewei@gmail.com>
2019-06-25 20:44:57 +0800

    与目前最新的gpc结构适配

--- summary/filter_by_area.m
+++ summary/filter_by_area.m
@@ -1,4 +1,4 @@
-function [dtr,f_gt,f_fp,gt,fp] = filter_by_area(R,top,bottom)
+function [dtr,f_gt,f_fp,gt,fp] = filter_by_area(R,gpc)
 dtr = R.dtr;
 f_gt = zeros(numel(dtr),1);
 f_fp = zeros(numel(dtr),1);
@@ -11,7 +11,7 @@ for i=1:numel(dtr)
     [~,yc] = box2center(dtr{i});
     gti = dtr{i}(:,6)==1;
     fpi = dtr{i}(:,6)==0;
-    outer = yc<top(i) | yc>bottom(i);
+    outer = yc<gpc{i}(2) | yc>gpc{i}(3);
     dtr{i}(:,6) = [];
     dtr{i}(outer,:)=[];
 
